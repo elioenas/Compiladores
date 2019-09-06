cc06ff9d6f8bda27f8db053c584b17dd93d04a57
Elioenas Batista de Sousa <30697363+elioenas@users.noreply.github.com>
2019-09-06 16:11:09 -0300

    Add files via upload

---
+++ AnalisadorLexico/src/br/com/analisadorlexico/GeradorClassesJflex.java
@@ -0,0 +1,12 @@
+package br.com.analisadorlexico;
+
+import java.io.File;
+
+public class GeradorClassesJflex {
+
+  public static void main(String[] args) {
+	
+    jflex.Main.generate( new File( "especificacoes/Minic.flex" ) );
+
+  }
+}
